f46b8e288c61e77e088c7963c760affa5b4ddd80
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-03-22 00:34:28 +0600

    mint nfts

--- contracts/Dappcord.sol
+++ contracts/Dappcord.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Dappcord is ERC721 {
     uint256 public totalChannels;
+    uint256 public totalSupply;
     address public owner;
 
     struct Channel {
@@ -14,15 +15,28 @@ contract Dappcord is ERC721 {
     }
 
     mapping(uint256 => Channel) public channels;
+    mapping(uint256 => mapping(address => bool)) public hasJoined;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
 
     constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {
         owner = msg.sender;
     }
 
-    function createChannel(string memory _name, uint256 _cost) public {
+    function createChannel(string memory _name, uint256 _cost) public onlyOwner{
+        require(msg.sender == owner);
         totalChannels++;
         channels[totalChannels] = Channel(totalChannels, _name, _cost);
 
+    }
+
+    function mint(uint256 _id) public payable{
+        hasJoined[_id][msg.sender] = true;
+        totalSupply ++;
+        _safeMint(msg.sender, totalSupply);
     }
 
     function getChannel(uint256 _id) public view returns (Channel memory) {
