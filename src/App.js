61042734f415dd82b10e2c1aaeab9759fa2b6e3d
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-03-23 02:59:40 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -19,10 +19,24 @@ const socket = io('ws://localhost:3030');
 
 function App() {
 
+  const [account, setAccount] = useState(null)
+
+  const loadBlockchainData = async () => {
+      window.ethereum.on('accountsChanged', async () => {
+      window.location.reload()
+      })
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  },[])
+
+
+
   return (
     <div>
-      <h1 style={{ textAlign: "center", padding: "15px" }}>Welcome to Dappcord</h1>
-
+        <Navigation account={account} setAccount={setAccount}/>
+            {/* <h1>{account}</h1> */}
       <main>
 
       </main>
