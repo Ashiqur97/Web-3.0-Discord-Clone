5b13aba1efae342c97e0e579dbfc892099f566dd
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-03-23 18:50:27 +0600

    chatting

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
   const [channels, setChannels] = useState([])
 
   const [currentChannel, setCurrentChannel] = useState(null)
-
+  const [messages, setMessages] = useState([])
   
   const loadBlockchainData = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum)
@@ -53,6 +53,25 @@ function App() {
 
   useEffect(() => {
     loadBlockchainData()
+
+    socket.on("connect", () => {
+      socket.emit('get messages')
+    })
+
+    socket.on('new message', (messages) => {
+      setMessages(messages)
+    })
+
+    socket.on('get messages', (messages) => {
+      setMessages(messages)
+    })
+
+    return () => {
+      socket.off('connect')
+      socket.off('new message')
+      socket.off('get messages')
+    }
+
   },[])
 
 
@@ -71,7 +90,7 @@ function App() {
              currentChannel={currentChannel}
              setCurrentChannel={setCurrentChannel}
           />
-          <Messages />
+          <Messages account={account} messages={messages} currentChannel={currentChannel} />
       </main>
     </div>
   );
