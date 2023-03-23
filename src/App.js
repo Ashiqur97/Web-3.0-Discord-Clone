a91f3aa14d4204a1b0c22e27bc1f8a5b5c58090c
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-03-23 17:37:23 +0600

    channels

--- src/App.js
+++ src/App.js
@@ -19,12 +19,36 @@ const socket = io('ws://localhost:3030');
 
 function App() {
 
+  const [provider, setProvider] = useState(null)
   const [account, setAccount] = useState(null)
 
+  const [dappcord, setDappcord] = useState(null)
+  const [channels, setChannels] = useState([])
+
+  const [currentChannel, setCurrentChannel] = useState(null)
+
+  
   const loadBlockchainData = async () => {
-      window.ethereum.on('accountsChanged', async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider)
+
+    const network = await provider.getNetwork()
+    const dappcord = new ethers.Contract(config[network.chainId].Dappcord.address, Dappcord, provider)
+    setDappcord(dappcord)
+
+    const totalChannels = await dappcord.totalChannels()
+    const channels = []
+
+    for (var i = 1; i <= totalChannels; i++) {
+      const channel = await dappcord.getChannel(i)
+      channels.push(channel)
+    }
+
+    setChannels(channels)
+
+    window.ethereum.on('accountsChanged', async () => {
       window.location.reload()
-      })
+    })
   }
 
   useEffect(() => {
@@ -38,7 +62,16 @@ function App() {
         <Navigation account={account} setAccount={setAccount}/>
             {/* <h1>{account}</h1> */}
       <main>
-
+          <Servers/> 
+          <Channels 
+             provider={provider}
+             account={account}
+             dappcord={dappcord}
+             channels={channels}
+             currentChannel={currentChannel}
+             setCurrentChannel={setCurrentChannel}
+          />
+          <Messages />
       </main>
     </div>
   );
